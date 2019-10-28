058bef45678b670043f777b7b91d297c15ed35d5
Isaac <isaacnoda@gmail.com>
2019-10-28 00:45:50 -0600

    Adds https redirect

--- app.rb
+++ app.rb
@@ -3,6 +3,11 @@ require './lib/pinger'
 
 class App < Sinatra::Base
   get "/" do
+    if ENV["RACK_ENV"] == "production" && request.scheme == "http"
+      redirect "https://#{request.host}"
+      return
+    end
+
     @sites = Pinger.sites
     @running = !Pinger.should_skip_pings
     erb :index
